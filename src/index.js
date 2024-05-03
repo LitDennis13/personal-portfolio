e1e3f250bb40ee1bccd5d3f3eead53518446e96c
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-05-02 22:38:51 -0600

    create header
    Header includes name, profile picture, and
    about me portion

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Style Sheets
+import "./Styles/styles.css";
+
+// Scripts
+import "./Scripts/image-loading-script";
+
+/*
+    header
+    footer
+    aside
+    main
+    nav
+    section
+    article
+*/
