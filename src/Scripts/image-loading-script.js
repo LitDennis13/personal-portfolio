4218fd751a4e606d8cf375fc2b4969641685d014
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-05-03 17:41:21 -0600

    update paths on image loading script

--- src/Scripts/image-loading-script.js
+++ src/Scripts/image-loading-script.js
@@ -1,16 +1,24 @@
-import mobileAvatar from "../images/Avatar-Mobile.png";
-import tabletAvatar from "../images/Avatar-Tablet.png";
-import desktopAvatar from "../images/Avatar-Desktop.png";
+import mobileAvatar from "../images/Avatars/Avatar-Mobile.png";
+import tabletAvatar from "../images/Avatars/Avatar-Tablet.png";
+import desktopAvatar from "../images/Avatars/Avatar-Desktop.png";
 
-import mobileAvatarComputer from "../images/AvatarComputer-Mobile.png";
+import mobileAvatarComputer from "../images/Avatars/AvatarComputer-Mobile.png";
 
-import tabletAvatarComputer from "../images/AvatarComputer-Tablet.png";
+import tabletAvatarComputer from "../images/Avatars/AvatarComputer-Tablet.png";
 
-import desktopAvatarComputer from "../images/AvatarComputer-Desktop.png";
+import desktopAvatarComputer from "../images/Avatars/AvatarComputer-Desktop.png";
 
-const mobileAvatarHtmlImg = document.querySelector("#mobile-profile-picture");
-mobileAvatarHtmlImg.src = mobileAvatar;
+function loadAvatarImage() {
+    // eslint-disable-next-line prettier/prettier
+    const mobileAvatarHtmlImg = document.querySelector("#mobile-profile-picture");
+    mobileAvatarHtmlImg.src = mobileAvatar;
+}
 
-// eslint-disable-next-line prettier/prettier
-const mobileAvatarComputerHtmlImg = document.querySelector("#mobile-profile-computer-picture");
-mobileAvatarComputerHtmlImg.src = mobileAvatarComputer;
+function loadAvatarComputerImage() {
+    // eslint-disable-next-line prettier/prettier
+    const mobileAvatarComputerHtmlImg = document.querySelector("#mobile-profile-computer-picture");
+    mobileAvatarComputerHtmlImg.src = mobileAvatarComputer;
+}
+
+// loadImages();
+export { loadAvatarImage, loadAvatarComputerImage };
