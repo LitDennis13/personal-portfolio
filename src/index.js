a6ec9253b43ba74608842e3934ec9898bac6b170
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-05-08 16:12:25 -0600

    update index.js imports

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 // Style Sheets
 import "./Styles/styles.css";
 import "./Styles/tablet-styles.css";
+import "./Styles/desktop-styles.css";
 
 // Scripts
 import "./Scripts/image-loading-script";
 import "./Scripts/load-about-me-page-script";
 import "./Scripts/load-projects-page-script";
 import "./Scripts/tab-switch-handler-script";
-/*
-    header
-    footer
-    aside
-    main
-    nav
-    section
-    article
-*/
